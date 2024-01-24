25f480de798ff54ac5f92b640e9e4bd06c98c158
aniket1702 <aniketmaurya1702@gmail.com>
2024-01-24 20:18:26 +0530

    Added QMetry Integration

---
+++ README.md
@@ -0,0 +1,8 @@
+# AutoScripter 🪄
+
+<div style="text-align: justify;">
+AutoScripter is a robust and reusable automation framework crafted for automating Web Pages. It leverages TestNG for effective test management and helps to run test methods in Parallel, Log4j2 for comprehensive logging, and integrates seamlessly with
+Allure for reporting. It supports both Local and Remote <b>(Selenium Grid and BrowserStack)</b> test execution. It has DataProvider mechanism which read data from Excel and executes the test based on the data provided.  
+</div>
+
+## Table of Content 🧾
--- src/test/java/com/autoscripter/pages/AdminPageTest.java
+++ src/test/java/com/autoscripter/pages/AdminPageTest.java
@@ -8,6 +8,8 @@ import com.autoscripter.pages.login.LoginPage;
 import io.qameta.allure.SeverityLevel;
 import org.testng.annotations.Test;
 
+import java.util.Map;
+
 public class AdminPageTest extends BaseTest {
 
     private final LoginPage loginPage = new LoginPage();
@@ -16,9 +18,9 @@ public class AdminPageTest extends BaseTest {
 
     @Test
     @AllureTest(story = "JIRA-12438", description = "Verify Admin Page with valid data", owner = "DEF XYZ",severity = SeverityLevel.MINOR)
-    public void adminPageTestValidData()
+    public void adminPageTestValidData(Map<String,String> data)
     {
-        loginPage.login("Admin","admin123");
+        loginPage.login(data.get("Username"), data.get("Password"));
         adminPage.clickHeader();
         adminPage.getTableData().getJSONObject(0);
     }
